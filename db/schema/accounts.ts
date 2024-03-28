ff0c8d5a267b4c152f4b3d72d7901d5e7d771c52
Leon Dore <leon.dore@gmail.com>
2024-03-28 14:31:08 -0400

    Work on accounts schema

--- db/schema/accounts.ts
+++ db/schema/accounts.ts
@@ -1,21 +1,32 @@
 import {
+  doublePrecision,
+  pgEnum,
   pgTable,
-  serial,
-  uniqueIndex,
+  text,
   uuid,
   varchar,
 } from 'drizzle-orm/pg-core';
 
-export const accounts = pgTable(
-  'account',
-  {
-    id: serial('id').primaryKey(),
-    uuid: uuid('uuid').unique().defaultRandom(),
-    name: varchar('name', { length: 256 }).notNull(),
-  },
-  (table) => {
-    return {
-      uuidIdx: uniqueIndex('uuid_idx').on(table.uuid),
-    };
-  }
+import { accountCategoryOptions } from '@/utils';
+
+// Enums
+export const accountCategoryEnum = pgEnum(
+  'account_category',
+  accountCategoryOptions
 );
+
+// Models
+export const accountCategories = pgTable('account_category', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: accountCategoryEnum('category').notNull(),
+  description: text('description'),
+});
+
+export const accounts = pgTable('account', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', { length: 256 }).notNull(),
+  balance: doublePrecision('balance').notNull().default(0),
+  description: text('description'),
+  categoryId: uuid('category_id').references(() => accountCategories.id),
+  accountNumber: varchar('account_number', { length: 64 }),
+});
---
+++ utils/constants.ts
@@ -0,0 +1,9 @@
+export const accountCategoryOptions = [
+  'cash',
+  'bank',
+  'creditCard',
+  'debitCard',
+  'investment',
+  'loan',
+  'other',
+] as const;
